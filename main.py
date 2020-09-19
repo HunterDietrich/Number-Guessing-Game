833d4eec0c28be5c66d2c904832297593159f333
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-19 00:10:48 +0000

    Added code

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-print("*****WELCOME TO TEH GUESSING GAME*****")
+import random
+print("*****WELCOME TO THE GUESSING GAME*****")
+
+
+print("I AM A COMPUTER, GUESS MY NUMBER")
+
+computerNum = random.randint(0,20)
+print(computerNum)
+
+#state variables
+win = False
+still_playing = True
+tries = 5
+
+while still_playing == True:
+    while tries > 0:
+        print()
+        playerNum = int(input("Enter a number between 0-20: "))
+
+        if playerNum > computerNum:
+            print("Your number is too big!")
+            tries -= 1
+            #tries = tries - 1
+            print("You have" + str(tries) + " tries left.")
+        elif playerNum < computerNum:
+            print("Your number is too small.")
+            tries -= 1
+            print("You have " + str(tries) + " tries left.")
+        else:
+            print("You guess the correct number!")
+            win = True
+            break
+    if win == True:
+        print("Congratulations! You won at " + str(6-tries)+ " tries")
+    else:
+        print("No more tries")
+        print("The number was " + str(computerNum))
+
+    answer = input("Do you want to play again? Y or N")
+
+    if answer == "N":
+        print("Okay! bye")
+        still_playing = False
+    else:
+        win = False
+        tries = 5
+        computerNum = random.randint(0, 20)
+
+
